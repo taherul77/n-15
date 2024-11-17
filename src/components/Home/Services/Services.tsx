20e40292ccb82168bdfb727b1f420a46300e3a78
SK Taherul Islam Taz <taherul15-2317@diu.edu.bd>
2024-11-17 16:21:05 +0600

    update project

--- src/components/Home/Services/Services.tsx
+++ src/components/Home/Services/Services.tsx
@@ -1,31 +1,29 @@
-
 import React from "react";
 import { BentoGrid, BentoGridItem } from "@/components/ui/bento-grid";
-import { FaCogs,FaMobileAlt } from "react-icons/fa";
+import { FaCogs, FaMobileAlt } from "react-icons/fa";
 import { TbWorldCog } from "react-icons/tb";
 import { CiMobile1 } from "react-icons/ci";
 
 const Services = () => {
-
   const items = [
     {
       title: "The Dawn of Innovation",
       description: "Explore the birth of groundbreaking ideas and inventions.",
-    
+
       className: "md:col-span-1",
       icon: <FaCogs className="h-4 w-4 ]" />,
     },
     {
       title: "The Digital Revolution",
       description: "Dive into the transformative power of technology.",
-    
+
       className: "md:col-span-1",
       icon: <FaMobileAlt className="h-4 w-4 " />,
     },
     {
       title: "The Art of Design",
       description: "Discover the beauty of thoughtful and functional design.",
-    
+
       className: "md:col-span-1",
       icon: <TbWorldCog className="h-4 w-4 " />,
     },
@@ -33,7 +31,7 @@ const Services = () => {
       title: "The Power of Communication",
       description:
         "Understand the impact of effective communication in our lives.",
-    
+
       className: "md:col-span-1",
       icon: <CiMobile1 className="h-4 w-4 " />,
     },
@@ -41,7 +39,7 @@ const Services = () => {
       title: "The Power of Communication",
       description:
         "Understand the impact of effective communication in our lives.",
-    
+
       className: "md:col-span-1",
       icon: <CiMobile1 className="h-4 w-4 " />,
     },
@@ -49,7 +47,7 @@ const Services = () => {
       title: "The Power of Communication",
       description:
         "Understand the impact of effective communication in our lives.",
-    
+
       className: "md:col-span-1",
       icon: <CiMobile1 className="h-4 w-4 " />,
     },
@@ -62,7 +60,6 @@ const Services = () => {
             key={i}
             title={item.title}
             description={item.description}
-         
             className={item.className}
             icon={item.icon}
           />
