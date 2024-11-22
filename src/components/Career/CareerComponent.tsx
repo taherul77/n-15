bb77a9825f1f0c1f33ea1cd99653d75b2e43be48
SK Taherul Islam Taz <taherul15-2317@diu.edu.bd>
2024-11-23 00:48:59 +0600

    update career component

--- src/components/Career/CareerComponent.tsx
+++ src/components/Career/CareerComponent.tsx
@@ -25,13 +25,13 @@ const CareerComponent = () => {
         <Section heading="Current Job Openings" />
       </div>
 
-      <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+      <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
         {items.map((item) => (
           <BackgroundGradient
             key={item.id}
             className="rounded-[22px] h-[300px]  p-6 bg-white dark:bg-zinc-900"
           >
-            <p className="text-base sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">
+            <p className="font-bold sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">
               {item.title}
             </p>
             <p className="text-sm text-neutral-600 dark:text-neutral-400  line-clamp-6	">
@@ -41,9 +41,9 @@ const CareerComponent = () => {
               whileHover={{ scale: 1.1 }}
               whileTap={{ scale: 0.9 }}
               transition={{ type: "spring", stiffness: 400, damping: 10 }}
-              className="rounded-full px-4 py-2 text-white flex items-center space-x-1 bg-gradient-to-r from-[#4B1E7F]  to-[#FC6617] mt-4 text-xs font-bold "
+              className="rounded-full uppercase px-4 py-2 text-white flex items-center space-x-1 bg-gradient-to-r from-[#4B1E7F]  to-[#FC6617] mt-4 text-xs font-bold "
             >
-              <span>Apply now</span>
+              <span >Apply now</span>
             </motion.button>
           </BackgroundGradient>
         ))}
