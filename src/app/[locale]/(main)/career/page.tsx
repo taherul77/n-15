a82270672e1565bb3a0a492f663ad24e7652f95b
SK Taherul Islam Taz <taherul15-2317@diu.edu.bd>
2024-11-30 12:50:52 +0600

    update

--- src/app/[locale]/(main)/career/page.tsx
+++ src/app/[locale]/(main)/career/page.tsx
@@ -4,7 +4,7 @@ const page = () => {
   return (
     <div className="max-w-[1120px] mx-auto min-h-screen">
       <div className="py-20">
-      <CareerComponent />
+        <CareerComponent />
       </div>
     </div>
   );
